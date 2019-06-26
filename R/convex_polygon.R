958ab032cae13b8fd4f742b0e569a07ee00449e3
Hannah <direch@uw.edu>
2019-06-25 17:05:07 -0700

    early drafts to find kernels

--- R/convex_polygon.R
+++ R/convex_polygon.R
@@ -5,23 +5,36 @@ attributes(K) <- list("pts" = cbind(c(1, 2, 3, 4, 5), c(5, 4, 3, 2, 1)))
 #' Get number of points in convex_poly object
 #' @param x \code{convex_poly} object
 #' @returns number of points 
-n <- function(x) {
+find_n <- function(x) {
   nrow(attr(K, "pts"))
 }
 
 #' Add a point to a convex_poly object
 #' @param x \code{convex_poly} object
-#' @param pos integer indicating in which position the new point shoule be added
-add_pt <- function(x, pos = 0) {
+#' @param pos integer indicating in which position the new point shouled be added
+add_pt <- function(x, pos) {
   stopifnot(class(x) == "convex_poly")
-  pts <- attr(K, "pts");
-  n <- nrow(pts)
+  pts <- attr(x, "pts");
+  n <- find_n(x)
   stopifnot(pos >= 0 & pos <= n + 1 & pos%%1 == 0)
   if (pos == 0) {
-    attr(K, "pts") <- rbind(new, pts)
+    attr(x, "pts") <- rbind(new, pts)
   } else if (pos == n + 1) {
-    attr(K, "pts") <- rbind(pts, new)
+    attr(x, "pts") <- rbind(pts, new)
   } else {
-    attr(K, "pts") <- rbind(pts[1:(pos - 1), ], new, pts[pos:n, ])
+    attr(x, "pts") <- rbind(pts[1:(pos - 1), ], new, pts[pos:n, ])
   }
+  return(x)
 }
+
+#' Remove a point form a convex_poly object
+#' @param x \code{convex_poly} object
+#' @param pos integer indicating which positions' point should be removed
+rm_pt <- function(x, pos) {
+  stopifnot(class(x) == "convex_poly")
+  pts <- attr(K, "pts")
+  n <- find_n(x) 
+  attr(x, "pts") <- pts[-pos,]
+  return(x)
+}
+
---
+++ R/planes_intersections_polys.R
@@ -0,0 +1,151 @@
+library('sp')
+
+#' Function to make a \code{SpatialPolygons} object
+#' @param coords n x 2 matrix of coordinates with first column giving 
+#' x-coordinates and second column giving y-coordinates
+#' @param name character string giving name of polygon  
+#' @importFrom sp Polygon, Polygons, SpatialPolygons
+make_poly <- function(coords, name) {
+  return(SpatialPolygons(list(Polygons(list(Polygon(coords)), name))))
+}
+
+
+#' Function to make a bounding box normalized to have side lengths of 1
+bbox <- function() {
+  coords <- rbind(c(0, 0), c(0, 1), c(1, 1), c(1, 0))
+  name <- 'bbox'
+  make_poly(coords, name)
+}
+
+
+# Make an interior half-plane given the first and last points of the edge
+# bounding box
+#' @param p1 vector of coordinates of first point
+#' @param p2 vector of coordinates of second point
+#' @param poly \code{SpatialPolygons} object giving the polygon of interest
+#' @param box \code{SpatialPolygons} object giving the area in which to 
+#' make the plane
+#' @importFrom rgeos gDifference
+int_half_plane <- function(p1, p2, poly, box) {
+  eps <- .01 #distance from 
+  
+  ###vertical line
+  if (p1[1] == p2[1]) {
+    half_plane <- make_poly(rbind(c(p1[1], 0), c(p1[1], 1), c(1, 1), c(1, 0)), 
+                            "half_plane")
+    mid <- (p1 + p2)/2
+    test_pt <- c(mid[1] - eps, mid[2])
+    test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    if (!gIntersects(test_pt, poly)) {
+      test_pt <- c(mid[1] + eps, mid[2])
+      test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    }
+  ###horizontal lines  
+  } else if (p1[2] == p2[2]) {
+    half_plane <- make_poly(rbind(c(0, p1[2]), c(1, p1[2]), c(1, 0), c(0, 0)), 
+                            "half_plane")
+    mid <- (p1 + p2)/2
+    test_pt <- c(mid[1], mid[2] - eps)
+    test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    if (!gIntersects(test_pt, poly)) {
+      test_pt <- c(mid[1], mid[2] + eps)
+      test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    }
+  ###typical case
+  } else {
+    ##find slope and intercept
+    m <- (p2[2]- p1[2])/(p2[1] - p1[1])
+    b <- p1[2] - m*p1[1]
+    
+    ##find the two points that intersect with the bounding box
+    #use > and < for x and >= and <= for y to avoid duplicates
+    bd_pts <- matrix(nrow = 0, ncol = 2)
+    cases <- rep(FALSE, 4) 
+    y_0 <- m*0 + b #case 1, y given x = 0
+    if (y_0 >= 0 & y_0 <= 1) {
+      bd_pts <- rbind(bd_pts, c(0, y_0))
+      cases[1] <- TRUE
+    } 
+    
+    y_1 <- m*1 + b #case 2, y given x = 1
+    if (y_1 >= 0 & y_1 <= 1) {
+      bd_pts <- rbind(bd_pts, c(1, y_1))
+      cases[2] <- TRUE
+    }
+   
+    x_0 <- -b/m #case 3, x given y = 0
+    if (x_0 > 0 & x_0 < 1) {
+      bd_pts <- rbind(bd_pts, c(x_0, 0))
+      cases[3] <- TRUE
+    }
+    
+    x_1 <- (1 - b)/m #case 4, x given y = 1
+    if (x_1 > 0 & x_1 < 1) {
+      bd_pts <- rbind(bd_pts, c(x_1, 1))
+      cases[4] <- TRUE
+    }
+    stopifnot(sum(cases) == 2)
+    
+    ###make possible half_plane by getting coordinates of polygon formed by 
+    #typical line and bounding box
+    if (cases[1] & cases[2]) {
+      half_plane <- rbind(bd_pts, c(1, 0), c(0, 0), bd_pts[1,])
+    } else if (cases[1] & cases[3]) {
+      half_plane <- rbind(bd_pts, c(0, 0), bd_pts[1,])
+    } else if (cases[1] & cases[4]) {
+      half_plane <- rbind(bd_pts, c(0, 1), bd_pts[1,])
+    } else if (cases[2] & cases[3]) {
+      half_plane <- rbind(bd_pts, c(1, 0), bd_pts[1,])
+    } else if (cases[2] & cases[4]) {
+      half_plane <- rbind(bd_pts, c(1, 1), bd_pts[1,])
+    } else if (cases[3] & cases[4]) {
+      half_plane <- rbind(bd_pts, c(0, 1), c(0, 0), bd_pts[1,])
+    }
+    half_plane <- make_poly(half_plane, "half_plane")
+    
+    ###make test point on one side of the midpoint
+    mid <- (p1 + p2)/2
+    #perpendicular line
+    m_perp <- -1/m
+    b_perp <- mid[2] - m_perp*mid[1]
+    test_pt <- c(mid[1] - eps, m_perp*(mid[1] - eps) + b_perp)
+    test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    if (!gIntersects(test_pt, poly)) {
+      test_pt <- c(mid[1] + eps, m_perp*(mid[1] + eps) + b_perp)
+      test_pt <- SpatialPoints(matrix(test_pt, ncol = 2))
+    }
+  }
+    
+  ###find interior half plane
+  if (gIntersects(test_pt, half_plane)) {
+    return(gIntersection(poly, half_plane))
+  } else {
+    return(gIntersection(poly, gDifference(box, half_plane)))
+  }
+}
+
+#' Function to find the kernel of a star-shaped polygon
+#' @coords matrix of dimension n x 2 that gives the coordinates of the 
+#' star-shaped polygon with the first column giving x-coordinates and the second
+#' column giving y-coordinates
+find_kernel <- function(coords) {
+  n_pts <- nrow(coords)
+  stopifnot(any(coords[1,] != coords[n_pts]))
+  bb <- bbox()
+  poly_curr <- make_poly(coords, "poly")
+  
+  #find the first plane, using the last and first point
+  kernel <- int_half_plane(p1 = coords[n_pts,], p2 = coords[1,], 
+                           poly = poly_curr,  box = bb)
+  
+  for (i in 1:(n_pts - 1)) {
+    temp_half_plane <- int_half_plane(p1 = coords[i,], p2 = coords[i + 1,], 
+                                      poly = poly_curr, box = bb)
+    kernel <- gIntersection(kernel, temp_half_plane)
+  }
+}
+
+
+
+
+
