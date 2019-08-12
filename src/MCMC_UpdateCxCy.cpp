7b779d1d26e038ee00f8decd6fd7dd19f087e694
Hannah <direch@uw.edu>
2019-08-12 16:39:12 -0700

    code now runs, need to add muC and SigmaC Gibbs steps, and test

--- src/MCMC_UpdateCxCy.cpp
+++ src/MCMC_UpdateCxCy.cpp
@@ -12,6 +12,7 @@ arma::mat mvrnormCpp(arma::vec mu, arma::mat Sigma) {
 
 // compute whether to accept or reject proposal based on
 // log of acceptance rate
+// [[Rcpp::export]]
 bool logAccept(double logR) {
   if (logR > 0.0) { //accept with prob min(r, 1)
     logR = 0.0;
@@ -62,7 +63,7 @@ double sumQFCon(arma::mat a, arma::mat b, arma::colvec c) {
   return(sum(0));
 }
 
-//repeate the same value
+//repeat the same value
 arma::colvec rep(double a, int n) {
   arma::colvec A = arma::zeros(n);
   for (int i = 0; i < n; i++) {
@@ -81,6 +82,17 @@ arma::mat sumQFCent(arma::mat y, arma::vec mu) {
     sum = sum + (y.col(i) - mu)*(y.col(i) - mu).t();
   }
   return(sum);
+}
+
+//compute and sum over quadratic form (y_{i} - mu)B(y_{i} - mu)'
+// [[Rcpp::export]]
+double sumQFCentSq(arma::mat y, arma::vec mu, arma::mat B) {
+  int n = y.n_cols;
+  arma::vec sum = arma::zeros(1);
+  for (unsigned i = 0; i < n; i++) {
+    sum = sum + (y.col(i) - mu).t()*B*(y.col(i) - mu);
+  }
+  return(sum(0));
 }
 
 //inverse wishart samples, computed as described in Hoff pg 110
@@ -108,19 +120,24 @@ double ptDist(arma::vec p1, arma::vec p2) {
   return(sqrt(arma::sum(arma::square(p1 - p2))));
 }
 
-// convert x values (locations of points on contours) and center point to
-// y values (distances from center points)
-// [[Rcpp::export]]
-arma::vec xToY(arma::mat x, arma::vec C) {
-  int n = x.n_cols;
-  arma::vec y =  arma::zeros(n);
-  for (unsigned i = 0; i < n; i++) {
-    y(i) = ptDist(x.col(i), C);
+//convert x values (locations of points on contours) and center point to
+//y values (distances from center points)
+//[[Rcpp::export]]
+arma::mat xToY(arma::cube x, arma::vec C) {
+  int nPts = x.n_cols;
+  int nSamp = x.n_slices;
+  arma::mat y = arma::zeros(nPts, nSamp);
+  for (unsigned i = 0; i < nSamp; i++) {
+    arma::mat xCurr = x.slice(i);
+    for (unsigned j = 0; j < nPts; j++) {
+      y(j, i) = ptDist(xCurr.col(j), C);
+    }
   }
   return(y);
 }
 
 
+ 
 //check if a point is in a polygon using the ray-casting approach
 //e.g., https://www.geeksforgeeks.org/how-to-check-if-a-given-point-lies-inside-a-polygon/
 // [[Rcpp::export]]
@@ -130,20 +147,20 @@ bool ptInPoly(arma::mat poly, arma::vec pt) {
 
   //main section: loop over the edges and count intersections with test ray
 
-  
+
   for (unsigned i = 0; i < n - 1; i++) {
     arma::vec ea = poly.col(i);
     arma::vec eb = poly.col(i + 1);
     //horizontal edge
-    if (ea(1) == eb(1)) {  
+    if (ea(1) == eb(1)) {
       if (ea(1) == pt(1)) { //test point and edge must be at same height for point to be in
         if (pt(0) >= std::min(ea(0), eb(0)) & pt(0) <= std::max(ea(0), eb(0))) {
           return(TRUE);
         }
       }
-    
-    //vertical edge  
-    } else if (ea(0) == eb(0)) { 
+
+    //vertical edge
+    } else if (ea(0) == eb(0)) {
       if (ea(0) > pt(0)) { //line must be to the right
         if ((pt(1) >= std::min(ea(1), eb(1))) &&
             (pt(1) <= std::max(ea(1), eb(1)))) {//test point y must be between edge end points y's
@@ -155,9 +172,9 @@ bool ptInPoly(arma::mat poly, arma::vec pt) {
           return(TRUE);
         }
       }
-      
-    //typical edge  
-    } else {  
+
+    //typical edge
+    } else {
       //find x-value at which the edge intersects the height of the test
       //ray. Then check if that x-value is on or to the right of the point
       double m = (eb(1) - ea(1))/(eb(0) - ea(0));
@@ -165,14 +182,13 @@ bool ptInPoly(arma::mat poly, arma::vec pt) {
       double xInter = (pt(1) - b)/m;
       if (abs(xInter - pt(0)) <= 1e-10) { //test point is (approximately) on edge
         return(TRUE);
-      } else if ((xInter > pt(0)) && (xInter >= std::min(ea(0), eb(0))) 
-            && (xInter <= std::max(ea(0), eb(0)))) { 
+      } else if ((xInter > pt(0)) && (xInter >= std::min(ea(0), eb(0)))
+            && (xInter <= std::max(ea(0), eb(0)))) {
         //x intersection needs to be on edge line segment and to the right of test pt
         count += 1;
       }
     }
   }
- 
   
   if (count % 2 == 0) {
     return(FALSE);
@@ -200,29 +216,41 @@ bool ptInPoly(arma::mat poly, arma::vec pt) {
 //'
 //' @return List of length X that gives BLAH
 // [[Rcpp::export]]
-List RunMCMC(int n_iter, int w, arma::mat x, arma::vec C,
+List RunMCMC(int n_iter, int w, arma::cube x, arma::vec C,
              arma::vec mu0,  arma::mat lambda0, arma::mat S0, int nu0,
-             arma::mat Sigma_ini,
+             arma::mat Sigma,
              arma::vec mu0C,  arma::mat lambda0C, arma::mat S0C, int nu0C,
-             arma::mat SigmaIniC) {
+             arma::mat SigmaC, 
+             double CxSD, double CySD, arma::mat kernHat) {
 
+  Rcout << "starting " << 0 << std::endl;
+  
   //constants
-  int nVecs = x.n_rows;
-  int nObs = x.n_cols;
+  int nVecs = x.n_cols;
+  int nObs = x.n_slices;
   int nuN = nu0 + nObs;
 
   //storage vectors and matrices
   arma::mat muStore(nVecs, n_iter/w);
   arma::cube sigmaStore(nVecs, nVecs, n_iter/w);
 
+  //Cx and Cy acceptance rates
+  double accRateCx = 0;
+  double accRateCy = 0;
+
   //inverses
-  arma::mat Sigma = Sigma_ini;
   arma::mat SigmaInv = inv(Sigma);
   arma::mat lambda0Inv = inv(lambda0);
 
-  //compute initial y
-  arma::vec y = xToY(x, C);
+  //compute initial y and muC
+  arma::mat y = xToY(x, C);
+  arma::vec muC = C;
+  
+  //create prop vectors
+  arma::vec CProp = C;
 
+  
+  //Rcout << "prelim done " << 0 << std::endl;
   // // /////////main loop/////////////
   for (unsigned j = 0; j < n_iter; j++) {
 
@@ -235,18 +263,95 @@ List RunMCMC(int n_iter, int w, arma::mat x, arma::vec C,
     if (j%w == 0) {
       muStore.col(j/w) = mu;
     }
-
+    //Rcout << "gibbs for mu done" << j << std::endl;
+  
    //////////Gibbs for Sigma/////////////
    arma::mat Sn = S0 + sumQFCent(y, mu);
    arma::mat Sigma = riwish(nuN, inv(Sn));
    if (j%w == 0) {
      sigmaStore.slice(j/w) = Sigma;
    }
+  //Rcout << "gibbs for sigma done" << j << std::endl;
+   
+  ////////////Metropolis for Cx///////
+  arma::vec Cx = arma::randn(1)*CxSD + C(0);
+  CProp(0) = Cx(0);
+  if (ptInPoly(kernHat, CProp)) {
+    //Rcout << "Cx after ptInpoly" << j << std::endl;
+    arma::mat yxProp = xToY(x, CProp);
+    bool acceptCx = false;
+    double logRCx = (- 0.5*sumQFCentSq(yxProp, mu, inv(Sigma))
+                     - 0.5*sumQFSq(CProp - muC, inv(SigmaC))
+                     + 0.5*sumQFCentSq(y, mu, inv(Sigma))
+                     + 0.5*sumQFSq(C - muC, inv(SigmaC)));
+    //Rcout << "have logRCx" << logRCx << std::endl;
+    acceptCx = logAccept(logRCx);
+    //Rcout << "acceptCx" << acceptCx << std::endl;
+    
+    if (acceptCx) {
+      //Rcout << "in accept Cx" << j << std::endl;
+      C = CProp;
+      accRateCx++;
+      y = yxProp;
+    } else {
+     //Rcout << "in reject Cx" << j << std::endl;
+      CProp = C;
+    }
+  } else {
+    //Rcout << "rejected bc ptInPoly Cx" << j << std::endl;
+    CProp = C;
   }
 
+  ////////////Metropolis for Cy///////
+  arma::vec Cy = arma::randn(1)*CySD + C(1);
+
+  CProp(1) = Cy(0);
+    if (ptInPoly(kernHat, CProp)) {
+    arma::mat yyProp = xToY(x, CProp);
+    bool acceptCy = false;
+    double logRCy = (- 0.5*sumQFCentSq(yyProp, mu, inv(Sigma))
+                     - 0.5*sumQFSq(CProp - muC, inv(SigmaC))
+                     + 0.5*sumQFCentSq(y, mu, inv(Sigma))
+                     + 0.5*sumQFSq(C - muC, inv(SigmaC)));
+    acceptCy = logAccept(logRCy);
+    //Rcout << "acceptCy" << acceptCy << std::endl;
+    
+    if (acceptCy) {
+      //Rcout << "in accept Cy" << j << std::endl;
+      C = CProp;
+      accRateCy++;
+      //Rcout << "accRateCy" << accRateCy << std::endl;
+      
+      y = yyProp;
+    } else {
+      //Rcout << "in reject Cy" << j << std::endl;
+      CProp = C;
+    }
+  } else {
+    //Rcout << "rejected bc ptInPoly" << j << std::endl;
+    CProp = C;
+  }
+  //Rcout << "metropolis for Cy done" << j << std::endl;
+  
+
+  ////////Gibbs for muC/////////////
+
+
+  //////////Gibbs for SigmaC/////////////
+
+
+
+  }
+
+  //finalize acceptance rate
+  accRateCx = accRateCx/n_iter;
+  accRateCy = accRateCy/n_iter;
+  
+  
   //return values
   List res;
-  res["mu"] = muStore; res["Sigma"] = sigmaStore;
+  res["mu"] = muStore; res["Sigma"] = sigmaStore; 
+  res["accRateCx"] = accRateCx; res["accRateCy"] = accRateCy;
   return(res);
 }
 
