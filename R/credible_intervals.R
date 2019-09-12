f4bb93310be5a6e51e251ce00c7be2b1dbcb9df8
Hannah <direch@uw.edu>
2019-09-12 13:43:50 -0700

    Adding function to compute cred regions

--- R/credible_intervals.R
+++ R/credible_intervals.R
@@ -1,3 +1,16 @@
+#function to find the credible region
+get_cred_regs <- function(prob, cred_ints) {
+  cred_regs <- list()
+  for (j in 1:length(cred_ints)) {
+    alpha <- (1 - cred_ints[j]/100)/2
+    in_int <- matrix(nrow = 100, ncol = 100, data = 0)
+    in_int[(prob >= alpha) & (prob <= (1 - alpha))] <- 1
+    cred_regs[[j]] <- conv_to_poly(in_int)
+  }
+  return(cred_regs)
+}
+
+
 
 
 #' Evaluate how regularly the 
@@ -9,7 +22,6 @@
 #' @param r maximum radius to make test lines
 #' @return vector of booleans indicating if crossing was in the credible interval
 #' 
-#TO DO: SIMPLIFY USING fixed_lines FUNCTION!
 eval_cred_reg <- function(truth, cred_reg, center, p, r = 5) {
   #generate testing lines
   theta <- seq(0, 2*pi, length = p + 1)
@@ -24,3 +36,35 @@ eval_cred_reg <- function(truth, cred_reg, center, p, r = 5) {
   }
   return(as.numeric(cover))
 }
+
+
+
+
+# 
+# 
+# #demo figure
+# #generate testing lines
+# pdf("/users/hdirector/desktop/metric_demo.pdf",
+#     height = 3, width = 6)
+# par(mfrow = c(1, 2))
+# for (p in c(20, 100)) {
+#   theta <- seq(0, 2*pi, length = p + 1)
+#   theta <- theta[1:p]
+#   x <- center[1] + r*cos(theta)
+#   y <- center[2] + r*sin(theta)
+#   plot(cred_reg, col = 'lightblue', main = sprintf("%i Test Lines", p),
+#        border = "white")
+#   points(cent_true[1], cent_true[2], col = 'blue', pch = 20)
+#   plot(truth, add = T, lwd = .5)
+#   for (i in 1:p) {
+#     test_line <- make_line(p1 = center, p2 = c(x[i], y[i]), "test")
+#     test_line <- gIntersection(cred_reg, test_line)
+#     if (gIntersects(truth, test_line)) {
+#       plot(test_line, col = "grey", add = TRUE, lwd = .5)
+#     } else {
+#       plot(test_line, col = 'red', add = TRUE, lwd = .5)
+#     }
+#   }
+# }
+# dev.off()
+# 
