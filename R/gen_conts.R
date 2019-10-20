e47455e84c9e9ab2e369f18c9846ac6fdeae4734
Hannah <direch@uw.edu>
2019-10-20 11:53:01 -0700

    splitting alpha/beta model out; warnings about neg. values

--- R/gen_conts.R
+++ R/gen_conts.R
@@ -7,9 +7,48 @@
 #' @param muCx parameter \eqn{muCx} in model
 #' @param muCy parameter \eqn{muCy} in model
 #' @param sigmaC2 parameter \eqn{sigmaC2} in model
+#' @param theta1 angle of first line
+gen_conts<- function(n_sim, mu, kappa, sigma, nu, muCx, muCy, sigmaC2,
+                     theta1) {
+  #preliminary
+  p <- length(mu)
+  theta_space <- 2*pi/p
+  theta_spacing <- theta_space*(0:(p-1))
+  theta <- compThetaDist(p, 2*pi/p)
+  Sigma <- compSigma(sigma, kappa, theta)
+  
+  #Simulate parallel points
+  y_sim <-  t(mvrnorm(n_sim, mu, Sigma))
+  y_sim[y_sim < 0] <- 0 #no negative lengths
+  thetas_sim <- matrix(rep(theta1 + theta_spacing, n_sim), ncol = n_sim)
+  paral <- array(dim = c(4, p, n_sim)) # dim1 = Cx_sim, Cy_sim, paral_x, paral_y, 
+  paral[1,,] <- matrix(rep(rnorm(n_sim, muCx, sqrt(sigmaC2)), each = p),
+                       ncol = n_sim)
+  paral[2,,] <- matrix(rep(rnorm(n_sim, muCy, sqrt(sigmaC2)), each = p),
+                       ncol = n_sim)
+  paral[3,,] <- y_sim*cos(thetas_sim) + paral[1,,]
+  paral[4,,] <- y_sim*sin(thetas_sim) + paral[2,,]
+  stopifnot(all(paral > 0))
+  coords <- apply(paral, 2:3, function(x){perpen_pt(nu, x[1], x[2], x[3],
+                                                    x[4])})
+  polys <- apply(coords, 3, function(x){make_poly(cbind(x[1,], x[2,]), "sim")})
+  return(list("coords" = coords, "polys" = polys))
+}
+
+
+
+#' simulate from contour model
+#' @param n_sim number of contours to simulate 
+#' @param mu parameter \eqn{mu} in model
+#' @param kappa parameter \eqn{kappa} in model
+#' @param sigma parameter \eqn{sigma} in model
+#' @param nu parameter \eqn{nu} in model
+#' @param muCx parameter \eqn{muCx} in model
+#' @param muCy parameter \eqn{muCy} in model
+#' @param sigmaC2 parameter \eqn{sigmaC2} in model
 #' @param alpha parameter \eqn{alpha} in model
 #' @param beta parameter \eqn{beta} in model
-gen_conts <- function(n_sim, mu, kappa, sigma, nu, muCx, muCy,
+gen_conts_rand_theta <- function(n_sim, mu, kappa, sigma, nu, muCx, muCy,
                       sigmaC2, alpha, beta) {
   #preliminary
   p <- length(mu)
