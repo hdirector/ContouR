9bb2fc1fb30a33063bf06a66cccb1faf881dd860
Hannah <direch@uw.edu>
2019-10-22 11:31:10 -0700

    swtich to script l

--- R/figure_code/method_components.R
+++ R/figure_code/method_components.R
@@ -1,21 +1,21 @@
 #######################################
 #Define generating lines, x, w, y, Cx, Cy
 #######################################
-#plot toy generating lines
+#plot toy generating linesa
 p <- 6; r <- .5
 theta_space <- (2*pi)/p
 thetas <- theta_space/2 + theta_space*(0:(p-1))
 Cx <- .5; Cy <- .5
 x_gen_line <- Cx + r*cos(thetas) 
 y_gen_line <- Cy + r*sin(thetas)
-#pdf("figures/method_components_demo.pdf")
+pdf("figures/method_components_demo.pdf")
 plot(0, 0, xlim = c(-.05, 1.05), ylim = c(-.05, 1.05), col = 'white', xlab = "", ylab = "",
      xaxt = "n", yaxt = "n")
 arrows(Cx, Cy, x_gen_line[1], y_gen_line[1])
 for (i in 2:p) {
   arrows(Cx, Cy, x_gen_line[i], y_gen_line[i])
 }
-text(x_gen_line[1] + .025, y_gen_line[1] + .01, expression('l'[1]))
+text(x_gen_line[1] + .025, y_gen_line[1] + .01, expression("\u2113"[1]))
 #plot Cx, Cy
 points(Cx, Cy, col = 'red', pch = 20, cex = 1.5)
 text(Cx - .015, Cy - .04, "C")
@@ -39,5 +39,5 @@ text(.64, .67, labels = "x")
 points(rbind(c(Cx, Cy), c(x_inter, y_inter)), type = "l", col = 'purple', 
        lwd = 2)
 text(.62, .535, "y")
-#dev.off()
+dev.off()
 
