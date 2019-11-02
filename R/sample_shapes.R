af65a5a05ee357a28eeaa24e10900894f89e3a94
Hannah <direch@uw.edu>
2019-11-02 14:14:58 -0700

    changing paral_lengths fn and altering sigmas

--- R/sample_shapes.R
+++ R/sample_shapes.R
@@ -20,11 +20,6 @@ p <- 30
 Cx <- .5; Cy <- .5
 theta_space <- 2*pi/p
 theta <- seq(theta_space/2, 2*pi, by = theta_space)
-theta <- theta[1:p]
-r <- .2
-spokes <- cbind(Cx + r*cos(theta), Cy + r*sin(theta))
-spokes <- apply(spokes, 1, function(x){make_line(p1 = rbind(x, c(Cx, Cy)), 
-                                                 name =  "spoke")})
 
 ###################
 #stop sign (convex)
@@ -35,25 +30,22 @@ template_stop <- rbind(c(.6, .5), c(.6, .55), c(.575, .575), c(.55, .6),
                       c(.4, .5), c(.4, .45), c(.425, .425), c(.45, .4), 
                       c(.5, .4), c(.55, .4), c(.575, .425), c(.6, .45))
 stop_poly <- make_poly(template_stop, "stop")
-
-#convert coordinates to parameters used in demos
-mu_stop <- sapply(spokes, function(x){SpatialLinesLengths(
-  gIntersection(x, stop_poly))})
+mu_stop <- paral_lengths(p, stop_poly, c(Cx, Cy))
 
 #other parameters
 kappa_stop <- 1
-sigma_stop <- rep(.005, p)
+sigma_stop <- c(rep(.005, 10), rep(.001, 10), rep(.007, 5), rep(.003, 5))
 
 #generate probability distribution
-gens_stop <- gen_conts_simp(n_sim = n_gen, mu_stop, kappa = kappa_stop,
-                            sigma_stop, Cx = Cx, Cy = Cy, theta[1])
+gens_stop <- gen_conts(n_sim = n_gen, mu_stop, kappa = kappa_stop,
+                       sigma_stop, Cx = Cx, Cy = Cy, theta[1])
 stop_bound <- sapply(gens_stop$polys, function(x){conv_to_grid(x, n_grid, n_grid)})
 stop_prob <- matrix(apply(stop_bound, 1, mean), nrow = n_grid, ncol = n_grid)
 
 #save shape parameters
 stop_sign <- list("mu" = mu_stop, "kappa" = kappa_stop, "sigma" = sigma_stop, 
                   "Cx" = Cx, "Cy" = Cy)
-save(stop_sign, file = "shape_pars/stop_sign.rda")
+#save(stop_sign, file = "shape_pars/stop_sign.rda")
 
 
 #####################
@@ -66,38 +58,30 @@ template_tie <- rbind(c(.6, .5), c(.6, .6), c(.5, .55),
 tie_poly <- make_poly(template_tie, "tie")
 
 #convert coordinates to parameters used in demos
-mu_tie <- sapply(spokes, function(x){SpatialLinesLengths(
-  gIntersection(x, tie_poly))})
+mu_tie <- paral_lengths(p, tie_poly, c(Cx, Cy))
 
 #other parameters
 kappa_tie <- 2
-sigma_tie <- c(seq(.008, .012, length = 8), seq(.012, .008, length = 7),
-                seq(.008, .012, length = 7), seq(.012, .008, length = 8))
+sigma_tie <- c(seq(.003, .012, length = 8), seq(.012, .003, length = 7),
+                seq(.003, .012, length = 7), seq(.012, .003, length = 8))
 
 #generate probability distribution
-gens_tie <- gen_conts_simp(n_sim = n_gen, mu_tie, kappa = kappa_tie,
-                            sigma_tie, Cx = Cx, Cy = Cy, theta[1])
+gens_tie <- gen_conts(n_sim = n_gen, mu_tie, kappa = kappa_tie,
+                      sigma = sigma_tie, Cx = Cx, Cy = Cy, theta[1])
 tie_bound <- sapply(gens_tie$polys, function(x){conv_to_grid(x, n_grid, n_grid)})
 tie_prob <- matrix(apply(tie_bound, 1, mean), nrow = n_grid, ncol = n_grid)
 
 
 #save shape parameters
 tie <- list("mu" = mu_tie, "kappa" = kappa_tie, "sigma" = sigma_tie, 
-                  "Cx" = Cx, "Cy" = Cy)
-save(tie, file = "shape_pars/tie.rda")
+            "Cx" = Cx, "Cy" = Cy)
+#save(tie, file = "shape_pars/tie.rda")
 
 
 ##############################
 #star (very non-convex)
 ##############################
 #write down coordinates of roughly-desired shape (a stop sign)
-# template_tree <- rbind(c(.52, .52), c(.58, .58), c(.51, .54),
-#                        c(.5, .6), c(.49, .54), c(.42, .58),
-#                        c(.48, .53),c(.42, .52), c(.48, .52),
-#                        c(.42, .48), c(.48, .5),
-#                        c(.48, .43), c(.52, .43), c(.52, .5),
-#                        c(.58, .48), c(.52, .51), c(.58, .52),
-#                        c(.52, .52))
 template_tree <- rbind(c(.52, .52), c(.64, .58), c(.51, .54),
                        c(.5, .6), c(.49, .54), c(.36, .58),
                        c(.48, .53),c(.36, .52), c(.48, .52), 
@@ -110,29 +94,31 @@ template_tree <- rbind(c(.52, .52), c(.64, .58), c(.51, .54),
 tree_poly <- make_poly(template_tree, "stop")
 
 #convert coordinates to parameters used in demos
-mu_tree <- sapply(spokes, function(x){SpatialLinesLengths(
-  gIntersection(x, tree_poly))})
+mu_tree <- paral_lengths(p, tree_poly, c(Cx, Cy))
+
 
 #other parameters
 kappa_tree <- 1
-sigma_tree <- rep(.01, p)
+sigma_tree <- c(rep(.01, 5), rep(.005, 5), rep(.015, 10), rep(.005, 5), 
+                rep(.01, 5))
+                
 
 #generate probability distribution
-gens_tree <- gen_conts_simp(n_sim = n_gen, mu_tree, kappa = kappa_tree,
-                            sigma_tree, Cx = Cx, Cy = Cy, theta[1])
+gens_tree <- gen_conts(n_sim = n_gen, mu_tree, kappa = kappa_tree,
+                       sigma = sigma_tree,  Cx = Cx, Cy = Cy, theta[1])
 tree_bound <- sapply(gens_tree$polys, function(x){conv_to_grid(x, n_grid, n_grid)})
 tree_prob <- matrix(apply(tree_bound, 1, mean), nrow = n_grid, ncol = n_grid)
 
 #save shape parameters
 tree <- list("mu" = mu_tree, "kappa" = kappa_tree, "sigma" = sigma_tree, 
             "Cx" = Cx, "Cy" = Cy)
-save(tree, file = "shape_pars/tree.rda")
+#save(tree, file = "shape_pars/tree.rda")
 
 
 ###################
 #shape figures
 ###################
-pdf("figures/sample_shapes_prob.pdf", height = 4, width = 8.5)
+#pdf("figures/sample_shapes_prob.pdf", height = 4, width = 8.5)
 set.panel()
 par(oma=c( 0,0,0,4)) # margin of 4 spaces width at right hand side
 set.panel(1,3) 
@@ -147,28 +133,28 @@ image(tree_prob, xlim = c(.3, .7), ylim = c(.3, .7), zlim = c(0, 1),
            main = "Tree")
 par(oma=c( 0,0,0,1))
 image.plot(legend.only=TRUE, zlim = c(0,1), col = viridis(10)) 
-dev.off()
+#dev.off()
 
 
 n_demo <- 15
-pdf("figures/sample_shapes_contours.pdf", height = 4, width = 8.5)
+#pdf("figures/sample_shapes_contours.pdf", height = 4, width = 8.5)
 set.panel(1,3) 
-plot(gens_stop$lines[[1]], xlim = c(.3, .7), ylim = c(.3, .7),
+plot(gens_stop$polys[[1]], xlim = c(.3, .7), ylim = c(.3, .7),
      lwd = .25, main = "Stop Sign")
 for (i in 2:n_demo) {
-  plot(gens_stop$lines[[i]], add = T, lwd = .15)
+  plot(gens_stop$polys[[i]], add = T, lwd = .15)
 }
-plot(gens_tie$lines[[1]],, xlim = c(.3, .7), ylim = c(.3, .7),
+plot(gens_tie$polys[[1]],, xlim = c(.3, .7), ylim = c(.3, .7),
      lwd = .25, main = "Bow Tie")
 for (i in 2:n_demo) {
-  plot(gens_tie$lines[[i]], add = T, lwd = .15)
+  plot(gens_tie$polys[[i]], add = T, lwd = .15)
 }
-plot(gens_tree$lines[[1]], xlim = c(.3, .7), ylim = c(.3, .7),
+plot(gens_tree$polys[[1]], xlim = c(.3, .7), ylim = c(.3, .7),
      lwd = .25, main = "Tree")
 for (i in 2:n_demo) {
-  plot(gens_tree$lines[[i]], add = T, lwd = .15)
+  plot(gens_tree$polys[[i]], add = T, lwd = .15)
 }
-dev.off()
+#dev.off()
 
 
 
