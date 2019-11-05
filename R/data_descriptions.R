4fe56f51d82061fc8124626589507cd16b55776b
Hannah <direch@uw.edu>
2019-11-05 13:58:37 -0800

    describe data

---
+++ R/data_descriptions.R
@@ -0,0 +1,51 @@
+#' Stop-sign Distributional Parameters
+#'
+#' Parameters to generate the distribution that looks like a stop sign. 
+#'
+#' @docType data
+#'
+#' @usage data(stop_sign)
+#'
+#' @format A list of parameters
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(stop)
+"stop_sign"
+
+#' Bow Tie Distributional Parameters
+#'
+#' Parameters to generate the distribution that looks like a stop sign. 
+#'
+#' @docType data
+#'
+#' @usage data(tie)
+#'
+#' @format A list of parameters
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(tie)
+"tie"
+
+
+#' Tree Distributional Parameters
+#'
+#' Parameters to generate the distribution that looks like a tree. 
+#'
+#' @docType data
+#'
+#' @usage data(tree)
+#'
+#' @format A list of parameters
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(tree)
+"tree"
