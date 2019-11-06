4aa76e36b568ed796220ae32efc9f023a060ebf8
Hannah <direch@uw.edu>
2019-11-06 11:26:51 -0800

    splitting apart fns related to fitting from misc fns

--- R/misc.R
+++ R/misc.R
@@ -69,21 +69,6 @@ coll_to_lines <- function(coll, ID = "line") {
 }
 
 
-#' Find angle distance between two angle measures in range (0, 2pi)
-#' @title Distance between angles
-#' @param a angle value in the range [0, 2pi]
-#' @param b angle value in the range [0, 2pi]
-ang_dist <- function(a, b) {
-  stopifnot(a >= 0); stopifnot(b >= 0)
-  stopifnot(a <= 2*pi); stopifnot(b <= 2*pi)
-  if (a == b) {
-    return(0)
-  } else {
-    p1 <- min(a, b); p2 <- max(a, b)
-    return(min(p2 - p1, p1 + (2*pi - p2)))
-  }
-}
-
 #' compute distance around the circle
 #' @param n number of points being considered
 dist_mat_circle <- function(n) {
@@ -142,29 +127,3 @@ angs_var <- function(C, coords) {
   sum_var_all <- sum(apply(angs, 2, function(x){var(diff(x))}))
   return(sum_var_all)
 }
-
-#' Find the center point based on at least two observations
-#' @param coords array of coordinates of dimensions 2 x number of points x
-#' number of samples
-find_center <- function(coords) {
-  stopifnot(dim(coords)[3] >= 2)
-  #make first line
-  p1a <- coords[,1,1]
-  p1b <- coords[,1,2]
-  m1 <- (p1a[2] - p1b[2])/(p1a[1] - p1b[1])
-  b1 <- p1a[2] - m1*p1a[1]
-  
-  #make second line
-  p2a <- coords[,2,1]
-  p2b <- coords[,2,2]
-  m2 <- (p2a[2] - p2b[2])/(p2a[1] - p2b[1])
-  b2 <- p2a[2] - m2*p2a[1]
-  
-  #find intersection
-  x_inter <- (b2 - b1)/(m1 - m2)
-  y_inter <- m1*x_inter + b1
-  
-  return(c(x_inter, y_inter))
-}
-  
-
