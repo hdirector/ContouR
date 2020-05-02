2ac89f16aa2d9a05d0b6f11322fa695e77891a33
Hannah <direch@uw.edu>
2020-05-02 12:59:35 -0700

    fixing edge cases with kernel

--- R/planes_intersections_polys.R
+++ R/planes_intersections_polys.R
@@ -49,7 +49,7 @@ bbox <- function(eps = 0) {
 #' make the plane
 #' @importFrom rgeos gDifference
 int_half_plane <- function(p1, p2, poly, box) {
-  eps <- .001 #how close do values need to be to horizontal or vertical 
+  eps <- 1e-5 #how close do values need to be to horizontal or vertical 
   ###vertical line
   if (p1[1] == p2[1]) {
     half_plane <- make_poly(rbind(c(p1[1], 0), c(p1[1], 1), c(1, 1), c(1, 0)), 
@@ -163,6 +163,7 @@ int_half_plane <- function(p1, p2, poly, box) {
 #' @param coords matrix of dimension n x 2 that gives the coordinates of the 
 #' star-shaped polygon with the first column giving x-coordinates and the second
 #' column giving y-coordinates
+#' @importFrom rgeos gSimplify
 find_kernel <- function(coords) {
   n_pts <- nrow(coords)
   bb <- bbox()
@@ -171,15 +172,29 @@ find_kernel <- function(coords) {
   #find the first plane, using the last and first point
   kernel <- int_half_plane(p1 = coords[n_pts,], p2 = coords[1,], 
                            poly = poly_curr,  box = bb)
+  if (gArea(kernel) < 1e-4) {
+    return(kernel)
+  }
   
   for (i in 1:(n_pts - 1)) {
     temp_half_plane <- int_half_plane(p1 = coords[i,], p2 = coords[i + 1,], 
                                       poly = poly_curr, box = bb)
-    kernel <- gIntersection(kernel, temp_half_plane)
+    if (gArea(temp_half_plane) > 1e-4) { #don't consider half planes on edge
+      kernel <- gIntersection(kernel, temp_half_plane)
+      if (suppressWarnings(!gIsValid(kernel))) { 
+        if (is(kernel)[1] == "SpatialCollections") {
+          kernel <- keep_poly(kernel)
+        } 
+        if (suppressWarnings(!gIsValid(kernel))) {
+          kernel <- gSimplify(kernel, tol = 0)
+        }
+      }
+      kernel <- keep_poly(kernel)
+    }
     #polygon has basically become a point, return it 
     #(otherwise will end up with tiny intersections due to computation)
-    if (gArea(kern) < 1e-4) {
-      return(kern)
+    if (gArea(kernel) < 1e-4) {
+      return(kernel)
     }
   }
   return(kernel)
@@ -194,8 +209,8 @@ find_kernel <- function(coords) {
 find_inter_kernel <- function(obs_coords) {
   n_obs <- length(obs_coords)
   for (i in 1:n_obs) {
-     kern_curr <- find_kernel(coords = rbind(obs$coords[[i]],
-                                             obs$coords[[i]][1,]))
+     kern_curr <- find_kernel(coords = rbind(obs_coords[[i]],
+                                             obs_coords[[i]][1,]))
     if (i == 1) {
       kern <- kern_curr
     } else {
@@ -211,50 +226,6 @@ find_inter_kernel <- function(obs_coords) {
   return(kern)
 }
 
-
-#' #' Make a 
-#' #' @param center coordinates of center point 
-#' #' @param n_lines how many fixed lines should be made
-#' #' @param bounds coordinates of the boundary of the region in an n x 2 matrix, 
-#' #' typically forming a rectangle
-#' #' @importFrom rgeos gIntersection
-#' fixed_lines <- function(center, n_lines,  
-#'                         bounds = rbind(c(0, 0), c(0, 1), c(1, 1), c(1, 0))) {
-#'   #Make a large upper bound for how far the radius should extend
-#'   r <- max(dist(bounds)) + 2
-#'   #generate testing lines
-#'   theta <- seq(0, 2*pi, length = n_lines + 1)
-#'   theta <- theta[1:n_lines]
-#'   bd_pts <- cbind(center[1] + r*cos(theta), center[2] + r*sin(theta))
-#'   lines <- apply(bd_pts, 1, function(w){make_line(p1 = center, 
-#'                                                    p2 = w, "line")})
-#'   return(lines)
-#' }
-
-#' #' Find the length on a 
-#' #' @param obs \code{SpatialPolygons} object giving the observed polygon
-#' #' @param lines a list of the fixed lines where each line is represented as
-#' #' a \code{SpatialLines} object
-#' #' @param center coordinates of center point 
-#' length_on_fixed <- function(obs, lines, center) {
-#'   obs_line <- as(obs, "SpatialLines")
-#'   n_lines <- length(lines)
-#'   lengths <- rep(NA, n_lines)
-#'   for (i in 1:n_lines) {
-#'     temp <- gIntersection(lines[[i]], obs_line)
-#'     #pull out coordinates
-#'     if (is(temp)[1] == "SpatialLines") { #unusual case
-#'       temp <- temp@lines[[1]]@Lines[[1]]@coords
-#'     } else { #typical case
-#'       temp <- temp@coords
-#'     }
-#'     temp <- temp[which.max(apply(temp, 1, function(x){get_dist(x, center)})),]
-#'     lengths[i] <- get_dist(temp, center)
-#'   }
-#'   return(lengths)  
-#' }
-
-
 #' Find distance between two points
 #' @param p1 coordinates of point 1
 #' @param p2 coordinates of point 2
@@ -263,40 +234,6 @@ get_dist <- function(p1, p2) {
 }
   
 
-#' #' Compute the \eqn{p} points parallel to the generating line
-#' #' @param poly \code{SpatialPolygons} object 
-#' #' @param p number of generating lines
-#' #' @param center vector of x and y coordinate of center point
-#' #' @param r max length line could extend
-#' paral_pts <- function(p, poly, center, r = 10) {
-#'   line <- as(poly, "SpatialLines")
-#'   theta_space <- 2*pi/p
-#'   theta <- seq(theta_space/2, 2*pi, by = theta_space)
-#'   outer_pts <- cbind(center[1] + r*cos(theta), center[2] + r*sin(theta))
-#'   gen_lines <- apply(outer_pts, 1, function(x){make_line(p1 = rbind(x, center), 
-#'                                                   name =  "spoke")})
-#'   pts <- sapply(gen_lines, function(x){gIntersection(x, line)})
-#'   pts <- sapply(pts, function(x){x@coords})
-#'   return(pts)
-#' }
-
-#' #' Compute lengths of lines from a point C to the edge of a polygon
-#' #' @param p number of lines
-#' #' @param poly \code{SpatialPolygons} object 
-#' #' @param center vector of length 2 giving points of C
-#' #' @param r maximum radius to consider
-#' #' @export
-#' paral_lengths <- function(p, poly, C, r = 10) {
-#'   theta_space <- 2*pi/p
-#'   thetas <- seq(theta_space/2, 2*pi, by = theta_space)
-#'   outer <- cbind(C[1] + r*cos(thetas), C[2] + r*sin(thetas))
-#'   lines <- apply(outer, 1, function(x){make_line(x, C, "ID")})
-#'   mu <- sapply(lines, function(x){SpatialLinesLengths(gIntersection(poly, x))})
-#'   return(mu)
-#' } 
-  
-  
-  
 #' Keep only the polygon from a \code{SpatialCollections} object
 #' @param poly \code{SpatialPolygons} or \code{SpatialCollections} object
 keep_poly <- function(poly) {
